715c341afb2a926ac72879b28f52c196bfc47c75
Tonny <musembitonnyj@gmail.com>
2022-07-29 11:51:01 +0300

    add

--- config/app.php
+++ config/app.php
@@ -177,6 +177,10 @@ return [
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
 
+
+        // Jorenvh\Share\Providers\ShareServiceProvider::class,
+
+
     ],
 
     /*
@@ -231,6 +235,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        // 'Share' => Jorenvh\Share\ShareFacade::class,
+
 
     ],
 
