140aa4257648c163a1ad7c2347dc69700c9ea1a5
Tonny <musembitonnyj@gmail.com>
2022-08-02 11:09:34 +0300

    add

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -7,4 +7,13 @@ use Illuminate\Http\Request;
 class SmsController extends Controller
 {
     //
+    public function index(){
+        return view('');
+    }
+    public function store(){
+        return view('');
+    }
+    public function destroy(){
+        return response()->json('$');
+    }
 }
