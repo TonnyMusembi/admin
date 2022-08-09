241fe127a4dcfbe3fb02ac5fa09abd24c595e794
Tonny <musembitonnyj@gmail.com>
2022-08-10 00:15:27 +0300

    add

---
+++ app/Http/Controllers/PDFController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Webklex\PDFMerger\Facades\PDFMergerFacade as PDFMerger;
+
+
+class PDFController extends Controller
+{
+    //
+    public function index(){
+        return view ('mergePDF');
+    }
+
+    public function store(Request $request){
+
+        $this->validate($request, [
+            'filenames' => 'required',
+            'filenames.*' => 'mimes:pdf'
+    ]);
+
+     if($request->hasFile('filenames')){
+        $pdf = PDFMerger::init();
+
+        foreach ($request->file('filenames') as $key => $value) {
+            $pdf->addPDF($value->getPathName(), 'all');
+        }
+
+        $fileName = time().'.pdf';
+        $pdf->merge();
+        $pdf->save(public_path($fileName));
+
+    }
+
+    return response()->download(public_path($fileName));
+
+    }
+}
