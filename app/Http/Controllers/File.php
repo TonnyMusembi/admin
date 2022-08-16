0057dd65bc7b59bc2d973b0d15809c83933564db
Tonny <musembitonnyj@gmail.com>
2022-08-16 12:32:35 +0300

    add

---
+++ app/Http/Controllers/File.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+// use App\Models\File;
+use Illuminate\Support\Facades\Validator;
+
+class File extends Controller
+{
+    //
+    public function index(){
+
+    }
+public function store(){
+
+}
+}
---
+++ app/Models/File.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
+class File extends Model
+{
+    use HasFactory;
+
+
+
+    protected $fillable = [
+        'title', 'name'
+
+
+    ];
+
+//     protected function name(): Attribute
+//     {
+//         return Attribute::make(
+//             get: fn ($value) => url('uploads/'.$value),
+//         );
+//     }
+ }
