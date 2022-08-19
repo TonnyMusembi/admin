34b9265926727e63de33d6f9d3e8958af4ab1249
Tonny <musembitonnyj@gmail.com>
2022-08-19 10:37:41 +0300

    add

---
+++ app/Http/Controllers/EmployeesController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Exports\EmployeesExport;
+use App\Exports\EmployeesByAgeExport;
+use Maatwebsite\Excel\Facades\Excel;
+
+class EmployeesController extends Controller
+{
+    //
+    public function index(){
+        return view('index');
+
+    }
+
+    public function exportCsv(){
+
+        $file_name = 'employees_'.date('Y_m_d_H_i_s').'.csv';
+        return Excel::download(new EmployeesExport, $file_name);
+    }
+    public function exportExcel(){
+        $file_name = 'employees_'.date('Y_m_d_H_i_s').'.xlsx';
+        return Excel::download(new EmployeesExport, $file_name);
+     }
+
+    public function exportByAgeCSV(Request $request){
+          $age =$request->age;
+          $file_name = 'employees_'.date('Y_m_d_H_i_s').'.csv';
+          return Excel::download(new EmployeesByAgeExport($age), $file_name);
+    }
+    public function store(){
+
+    }
+
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <title>Export data </title>
+
+   <!-- CSS -->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body>
+
+   <div class="container mt-5">
+
+      <a class="btn btn-primary" href="{{ route('employees.exportcsv') }}">CSV Export</a> &nbsp;&nbsp;
+      <a class="btn btn-primary" href="{{ route('employees.exportexcel') }}">Excel Export</a><br><br>
+
+      <form method='post' action="{{ route('employees.exportbyagecsv') }}">
+         @csrf
+         <div class="mb-3">
+            <label for="age" class="form-label">Age</label>
+            <input type="number" class="form-control" id="age" name="age" value="0">
+         </div>
+
+         <button type="submit" class="btn btn-success">Export</button>
+      </form>
+   </div>
+
+</body>
+</html
