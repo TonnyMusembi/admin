19acfaa32b25aa279b9af119278c0df59bb32552
TonnyMusembi <musembutonnyj@gmail.com>
2022-09-07 10:35:49 +0300

    add

---
+++ app/Http/Controllers/AcademicYearController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AcademicYearController extends Controller
+{
+    //
+    public function index(){
+        return view('pages.index');
+    }
+
+
+    public function store(Request $request){
+
+
+    }
+}
