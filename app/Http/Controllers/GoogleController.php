824f824442e4ac0da242030d4b8c95438f402817
TonnyMusembi <musembutonnyj@gmail.com>
2022-09-14 16:23:35 +0300

    add

---
+++ app/Http/Controllers/GoogleController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Visitor;
+
+
+class GoogleController extends Controller
+{
+    //
+    public function index(){
+        return view('result');
+
+    }
+    public function googleLineChart()
+    {
+        $visitors = Visitor::select("visit_date", "click", "viewer")->get();
+
+        $result[] = ['Dates','Clicks','Viewers'];
+        foreach ($visitors as $key => $value) {
+            $result[++$key] = [$value->visit_date, (int)$value->click, (int)$value->viewer];
+        }
+
+        return view('googleChart', compact('result'));
+    }
+}
