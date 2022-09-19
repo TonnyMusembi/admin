ab669cd8dca3a67f1b0fc327b9a8948da2aa7c94
TonnyMusembi <musembutonnyj@gmail.com>
2022-09-19 17:56:20 +0300

    add

---
+++ app/Http/Controllers/LeagueController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LeagueController extends Controller
+{
+    //
+    public function index(){
+        return view('');
+
+    }
+    public function store(Request $request){
+        return response()->json(['']);
+
+    }
+    public function create($id){
+
+    }
+
+}
